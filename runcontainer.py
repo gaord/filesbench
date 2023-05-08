d9ad42ed56688bfe77f4cf1d9bd6afa1656d0c9c
Ben Gao <bengao168@msn.com>
2023-05-08 22:28:51 +0800

    删除目录前等待子进程结束。格式调整。

--- runcontainer.py
+++ runcontainer.py
@@ -15,25 +15,30 @@ ceph_user = "admin"
 cephfs_path = "/volumes/testbench/smallfiles"
 
 # Set the base mount path for the directories
-mount_path = "/mnt/dir_"
-
+mount_path = "/mnt/ceph_client_"
+file_batch = "20w100g"
 # Set the root directory for the files
-root_dir = "/mnt/dir_1/10f53653-b7fe-4fd3-9288-c152a0c076fb/20w50g4d"
+root_dir = f"/mnt/mymount{cephfs_path}/4b8cf0c9-7c3e-44b5-882e-dc2ab1657513/{file_batch}"
 
 # Set the number of mount points to create
 num_mounts = 401
 
 def create_dir_mount(ceph_mon_ip, ceph_secret, ceph_user, cephfs_path, mount_path):
     # Create num_mounts directories under /mnt
+    print("create and mount directory/cephfs")
     for i in range(1, num_mounts):
         os.makedirs(f"{mount_path}{i}")
     
     # Mount CephFS file system on each directory
     for i in range(1, num_mounts):
-        os.system(f"mount -t ceph {ceph_mon_ip}:{cephfs_path} {mount_path}{i} -o secret={ceph_secret},name={ceph_user}")        
+        #使用fuse或者kernel moun
+        os.system(f"ceph-fuse --id {ceph_user} {mount_path}{i}")
+        #os.system(f"mount -t ceph {ceph_mon_ip}:{cephfs_path} {mount_path}{i} -o secret={ceph_secret},name={ceph_user}")        
+        
 
 def remove_dir_mount(mount_path):
     # Unmount and remove each directory
+    print("umount and remove cephfs/directory")
     for i in range(1, num_mounts):
         os.system(f"umount {mount_path}{i}")
         os.rmdir(f"{mount_path}{i}")
@@ -53,11 +58,16 @@ directories = [f for f in relative_paths if os.path.isdir(f)]
 # Select a random sample of directories
 selected_directories = random.sample(directories, num_mounts)
 
-# Run specified Python script on each mounted directory
+# Asynchronously simulate multiple client accesses
+processes = []
 for i in range(1, num_mounts):
-    # Asynchronously simulate multiple client accesses
-    cmd_to_run = ["python3", "randrwbench.py", "--sample_rate", "1.0", "--working_dir", "{}{}/10f53653-b7fe-4fd3-9288-c152a0c076fb/20w50g4d/{}".format(mount_path, i, os.path.basename(selected_directories[i-1]))]
+    cmd_to_run = ["python3", "randrwbench.py", "--sample_rate", "1", "--working_dir", "{}{}{}/4b8cf0c9-7c3e-44b5-882e-dc2ab1657513/{}/{}".format(mount_path, i, cephfs_path, file_batch, os.path.basename(selected_directories[i-1]))]
     process = subprocess.Popen(cmd_to_run)
+    processes.append(process)
+
+# Wait for all subprocesses to complete
+for process in processes:
+    process.wait()
 
 # Remove the directories and unmount the file system
 remove_dir_mount(mount_path)
